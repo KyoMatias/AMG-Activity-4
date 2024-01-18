f79292d169cdeeec61275ce03bbdc9af1ff582f5
Kyoko <108702599+KyoMatias@users.noreply.github.com>
2024-01-18 20:51:13 +0800

    Update BezierCurve.cs

--- Activity4/Assets/Scenes/BezierCurve.cs
+++ Activity4/Assets/Scenes/BezierCurve.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 
 public static class BezierCurve
 {
+    /// <summary>
+    /// PERFORMS  quadratic lerp
+    /// </summary>
+    /// <param name="p0">point 1</param>
+    /// <param name="p1">point 2</param>
+    /// <param name="p2">point 3</param>
+    /// <param name="t"> value should be between 0 and 1</param>
+    /// <returns></returns>
     public static Vector3 QuadraticLerp(Vector3 p0, Vector3 p1, Vector3 p2, float t) // This Function is responsible for creating the variables for the Vector3 vertices.
     {
             var clampedTime -Mathf.Clamp01(t);
